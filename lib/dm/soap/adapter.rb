98bbabe1592101add6ae4fe63aeb381e2add5566
Brandon Cox <brandoncox@new-host.home>
2013-04-17 11:42:21 -0400

    initial commit

---
+++ lib/dm/soap/adapter.rb
@@ -0,0 +1,9 @@
+require "dm/soap/adapter/version"
+
+module Dm
+  module Soap
+    module Adapter
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/dm/soap/adapter/version.rb
@@ -0,0 +1,7 @@
+module Dm
+  module Soap
+    module Adapter
+      VERSION = "0.0.1"
+    end
+  end
+end
