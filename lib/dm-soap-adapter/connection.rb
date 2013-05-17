cbd708a8b8943fc81a126bf23f3c16b766ede9af
Brandon Cox <brandoncox@admins-MacBook-Pro-3.local>
2013-05-16 23:31:50 -0400

    removed a lot of junk code and refactors SOAP calls to use Savon.

--- lib/dm-soap-adapter/connection.rb
+++ lib/dm-soap-adapter/connection.rb
@@ -1,21 +1,13 @@
 require 'dm-soap-adapter/connection/errors'
+require 'savon' 
 
 class SoapAdapter
   class Connection
     include Errors
 
-    def initialize(username, password, wsdl_path, api_dir)
-      @wrapper = SoapWrapper.new("Generic", "SOAP", wsdl_path, api_dir)
-      @username, @password = URI.unescape(username), password
-    end
-    attr_reader :user_id, :user_details
-
-    def wsdl_path
-      @wrapper.wsdl_path
-    end
-
-    def api_dir
-      @wrapper.api_dir
+    def initialize(options)
+      @client = Savon.client(wsdl: options['path'])
+      @options = options
     end
 
     def make_object(klass_name, values)
@@ -31,83 +23,23 @@ class SoapAdapter
       obj
     end
 
-    def field_name_for(klass_name, column)
-      klass = Object.const_get(klass_name)
-      fields = [column, Inflector.camelize(column.to_s), "#{Inflector.camelize(column.to_s)}__c", "#{column}__c".downcase]
-      options = /^(#{fields.join("|")})$/i
-      matches = []
-      klass.instance_methods.each{ |x| if fields.include? x.to_s then matches << x end }
-      if matches.any?
-        matches.first
-      else
-        raise FieldNotFound,
-            "You specified #{column} as a field, but none of the expected field names exist: #{fields.join(", ")}. " \
-            "Either manually specify the field name with :field, or check to make sure you have " \
-            "provided a correct field name."
-      end
-    end
-
-    def query(string)
-      with_reconnection do
-        require 'debugger'
-        debugger
-        res = driver.query(:queryString => string).result
-        records = res.records
-        while !res.done
-          res = driver.queryMore(:queryLocator => res.queryLocator).result
-          records += res.records
-        end
-        res.records = records 
-        res
-      end
-    rescue SOAP::FaultError => e
-      raise QueryError.new(e.message, [])
-    end
-
     def create(objects)
-      call_api(:create, CreateError, "creating", objects)
+      require 'debugger'
+      debugger
+      response = @client.call(@options[:create].to_sym, message: objects)
+      return response.body
     end
 
     def update(objects)
-      call_api(:update, UpdateError, "updating", objects)
+      response = @client.call(@options[:update].to_sym, message: objects)
+      return response.body
     end
 
     def delete(keys)
-      call_api(:delete, DeleteError, "deleting", keys)
-    end
-
-    private
-
-    def driver
-      @wrapper.driver
-    end
-
-    def call_api(method, exception_class, message, args)
-      with_reconnection do
-        result = driver.send(method, args)
-        if result.all? {|r| r.success}
-          result
-        else
-          # TODO: be smarter about exceptions here
-          raise exception_class.new("Got some errors while #{message} Salesforce objects", result)
-        end
-      end
-    end
-
-    def with_reconnection(&block)
-      yield
-      rescue SOAP::FaultError => error
-      retry_count ||= 0
-
-      case error.faultcode.text
-      when "sf:INVALID_SESSION_ID" then
-        DataMapper.logger.debug "Got a invalid session id; reconnecting" if DataMapper.logger
-        @driver = nil
-        login
-        retry_count += 1
-        retry unless retry_count > 5
-      else raise error
-      end
+      response = @client.call(@options[:delete].to_sym, message: keys)
+      return response.body
     end
   end
 end
+
+
