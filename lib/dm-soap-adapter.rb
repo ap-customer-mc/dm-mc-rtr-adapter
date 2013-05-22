e4f1d16bb28d5af50d30429df462c4c6c875f739
Brandon Cox <brandoncox@new-host-28.home>
2013-05-22 11:57:58 -0400

    removed the unused files

--- lib/dm-soap-adapter.rb
+++ lib/dm-soap-adapter.rb
@@ -2,10 +2,6 @@ require 'data_objects'
 require 'dm-core'
 require 'dm-validations'
 
-require 'soap/wsdlDriver'
-require 'soap/header/simplehandler'
-require 'rexml/element'
-
 require 'dm-soap-adapter/resource'
 require 'dm-soap-adapter/connection'
 require 'dm-soap-adapter/connection/errors'
@@ -13,7 +9,6 @@ require 'dm-soap-adapter/soap_wrapper'
 require 'dm-soap-adapter/version'
 require 'dm-soap-adapter/adapter'
 require 'dm-soap-adapter/property'
-require 'dm-soap-adapter/model_mapper'
 
 ::DataMapper::Adapters::SoapAdapter = DataMapperSoap::Adapter
 ::DataMapper::Adapters.const_added(:SoapAdapter)
